abcae9e395c32d29e70ecd63f9d3063a9dc02b2a
verabutler <verabutlerdesign@gmail.com>
2018-09-26 22:41:26 -0400

    add robot cards

---
+++ src/robots.js
@@ -0,0 +1,72 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham' ,
+        username: 'Bret',
+        email:'Sincere@april.biz',
+
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antonete',
+        email: 'Shanna@elissa.tv',
+
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Shiney',
+        email: 's.farre@gmail.com',
+
+    },
+    {
+        id: 4,
+        name: 'Metal Gear',
+        username: 'Solid',
+        email: '5901@aye.com',
+
+    },
+    {
+        id: 5,
+        name: 'Brace Yourself',
+        username: 'Crash',
+        email: 'make.it@go.ro',
+
+    },
+    {
+        id: 6,
+        name: 'Pretty Penny',
+        username: 'Julia',
+        email: 'sparkle.girl@cove.co',
+
+    },
+    {
+        id: 7,
+        name: 'Rainbow Arc',
+        username: 'Happy',
+        email: 'rain.maker@yo.org',
+
+    },
+    {
+        id: 8,
+        name: 'Random Joe',
+        username: 'Coffee',
+        email: 'waker.cup@fresh.ahh',
+
+    },
+    {
+        id: 9,
+        name: 'Lookee Looee',
+        username: 'Eyes',
+        email: 'watching.you@glasses.ln',
+
+    },
+    {
+        id: 10,
+        name: 'Yes Please',
+        username: 'No-Thanks',
+        email: 'You.Will@makeit.com',
+
+    },
+]
