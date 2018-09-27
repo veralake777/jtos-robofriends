c678cce83a08f2c753f8693c497d5da87520a53b
verabutler <verabutlerdesign@gmail.com>
2018-09-26 23:05:31 -0400

    add search feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import {robots} from './robots';
 
-const App  = () => {
-    return(
-        <CardList robots={robots}/>
-    )
+const state = {
+    
+}
+class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            robots: robots, 
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (e) => {
+        this.setState({ searchfield: e.target.value })
+    }
+    render(){
+        const filterRobots = this.state.robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return(
+            
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filterRobots}/>
+            </div>
+        )
+    }
+    
 
 }
 
