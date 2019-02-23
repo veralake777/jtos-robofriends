df22f8abc5368d7a9d357033272dfb3fa08e8d57
Vera Butler <verabutlerdesign@gmail.com>
2019-02-23 10:32:55 -0500

    Updates

---
+++ precache-manifest.d546228235a4312e80078d5d36ce7422.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "29a8e065b769a0c52f34",
+    "url": "/static/css/main.c92275fa.chunk.css"
+  },
+  {
+    "revision": "29a8e065b769a0c52f34",
+    "url": "/static/js/main.29a8e065.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "35b06138952856caaa5c",
+    "url": "/static/css/2.d37d14eb.chunk.css"
+  },
+  {
+    "revision": "35b06138952856caaa5c",
+    "url": "/static/js/2.35b06138.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "7b4a5e270361ff3ba747c46da8a65a88",
+    "url": "/index.html"
+  }
+];
